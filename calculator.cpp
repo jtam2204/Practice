f6c419a2d6c24027ad11d4fb329263190c3fce66
jtam2204 <jtam2204@gmail.com>
2023-06-03 15:08:20 +0100

    first commit

---
+++ calculator.cpp
@@ -0,0 +1,87 @@
+#include <iostream> 
+#include <stdlib.h>
+#include <sstream>
+using namespace std;
+
+
+float add(float a, float b){
+    return a+b;
+}
+float subtract(float a, float b){
+    return a-b;
+}
+float multiply(float a, float b){
+    return a*b;
+}
+float division(float a, float b){
+    return a/b;
+}
+
+int main(){
+    char olist[10];
+    float result=0, plist[10];
+    string list[10], input;
+    int optr=0, pptr=0, lptr=0;
+    cout<<"Please enter the equation: (no brackets and only +,-,*,/ operators)";
+    cin>>input;
+    for (int i = 0; i < input.length(); i++)
+    {
+        //cout<<i<<" input: "<<input[i]<<"\n";
+        if (input[i]!='+'&&input[i]!='-'&&input[i]!='*'&&input[i]!='/'){
+            list[lptr]+=input[i];
+            //cout<<lptr<<" list: "<<list[lptr]<<"\n";
+            if (input[i+1]=='+'||input[i+1]=='-'||input[i+1]=='*'||input[i+1]=='/'||i==input.length()-1){
+                stringstream num;
+                num<<list[lptr];
+                num>>plist[pptr];
+                //cout<<pptr<<" plist: "<<plist[pptr]<<"\n";
+                lptr++;
+                pptr++;
+                //cout<< "pptr: "<< pptr<<"\n";
+                //cout<< "optr: "<< optr<<"\n";
+                if (olist[optr-1]=='*'||olist[optr-1]=='/'){
+                    float num1=plist[pptr-2], num2=plist[pptr-1];
+                    //cout<<num1<<olist[optr-1]<<num2<<"\n";
+                    switch (olist[optr-1])
+                    {
+                    case '*':
+                        plist[pptr-2]=multiply(num1,num2);
+                        pptr--;
+                        break;
+                    case '/':
+                        plist[pptr-2]=division(num1,num2);
+                        pptr--;
+                        break;
+                    }
+                    optr--;
+                }
+            }
+        } else {
+            olist[optr]=input[i];
+            //cout<<optr<<" olist: "<<olist[optr]<<"\n";
+            optr++;
+            lptr++;
+        } 
+    }
+    pptr--;
+    optr--;
+    while (optr>=0)
+    {
+        //cout<<olist[optr]<<plist[pptr]<<"\n";
+        switch (olist[optr])
+        {
+        case '+':
+            plist[pptr-1]=add(plist[pptr-1],plist[pptr]);
+            pptr--;
+            break;
+        case '-':
+            plist[pptr-1]=subtract(plist[pptr-1],plist[pptr]);
+            pptr--;
+            break;
+        }
+        optr--;
+    }
+    result=plist[0];
+    cout<<"Result: "<<result<<"\n";   
+    return 0;
+}
